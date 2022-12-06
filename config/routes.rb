aabb291f1542740594379447efaa8bd9089dd1c6
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-07 01:14:53 +0300

    add menu-items/menu routes && controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :menu_items
-  resources :menus
+  resources :menu_items, only: [:index, :show, :create, :update, :destroy]
+  resources :menus, only: [:index, :show, :create, :update, :destroy]
   resources :customers
   resources :order_items
   resources :orders
-  resources :menu_items, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
