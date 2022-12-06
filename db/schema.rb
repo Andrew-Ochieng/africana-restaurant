6e5d5aaf26196702c9cd3e8648fd5c0d9b3530b8
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-06 23:41:46 +0300

    add menu_item seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_182807) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_193137) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,7 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_182807) do
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
-    t.integer "menu_item_id"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
