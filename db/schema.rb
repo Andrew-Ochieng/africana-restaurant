ec61acc984a1e5757cd6a2421539b3f335777c83
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-08 01:29:02 +0300

    add railway configurations

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_213913) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_201850) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "customers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password_hash"
-    t.string "password_salt"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "menu_items", force: :cascade do |t|
     t.string "name"
@@ -57,6 +46,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_213913) do
     t.integer "customer_id"
     t.integer "order_item_id"
     t.integer "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
