7d4297eb137e80e0314c5de0c3a48aa1021d42bd
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-09 01:32:05 +0300

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :address, presence: true
+    validates :password, presence: true, uniqueness: true
 
     has_many :orders
     has_many :order_items, through: :orders
