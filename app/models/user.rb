3db8a708695c62409f84276dc56b226438318b5e
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-09 10:57:05 +0300

    add application.rb bug fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :address, presence: true
-    validates :password, presence: true, uniqueness: true
+    validates :password, presence: true
 
     has_many :orders
     has_many :order_items, through: :orders
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,12 @@ module AfricanaApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    config.action_dispatch.cookies_same_site_protection = :strict
+
+    config.hosts << "https://afrikaan-restaurant-production.up.railway.app/"
   end
 end
